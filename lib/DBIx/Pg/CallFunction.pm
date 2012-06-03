947b6baa3d3fc87d67a4dd54e77e67fe37e27e3d
Joel Jacobson <joel@trustly.com>
2012-06-03 08:19:51 +0700

    Updated Documentation and made the call method private, renamed to _call.

--- lib/DBIx/Pg/CallFunction.pm
+++ lib/DBIx/Pg/CallFunction.pm
@@ -1,5 +1,5 @@
 package DBIx::Pg::CallFunction;
-our $VERSION = '0.007';
+our $VERSION = '0.008';
 use 5.008;
 
 =head1 NAME
@@ -8,7 +8,7 @@ DBIx::Pg::CallFunction - Simple interface for calling PostgreSQL functions from 
 
 =head1 VERSION
 
-version 0.007
+version 0.008
 
 =head1 SYNOPSIS
 
