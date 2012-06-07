ea2420802ee4aa5876ea0c3490ef32aa14b200ca
Joel Jacobson <joel@trustly.com>
2012-06-07 10:16:38 +0700

    Updated manifest

--- lib/DBIx/Pg/CallFunction.pm
+++ lib/DBIx/Pg/CallFunction.pm
@@ -1,5 +1,5 @@
 package DBIx::Pg::CallFunction;
-our $VERSION = '0.010';
+our $VERSION = '0.011';
 use 5.008;
 
 =head1 NAME
@@ -8,7 +8,7 @@ DBIx::Pg::CallFunction - Simple interface for calling PostgreSQL functions from 
 
 =head1 VERSION
 
-version 0.010
+version 0.011
 
 =head1 SYNOPSIS
 
